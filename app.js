ea876da510f1b3da8ccf83ebea6633aa40fbf9a9
nyambujoy <joyliznyambura@gmail.com>
2023-09-25 12:40:15 +0300

    added js

--- app.js
+++ app.js
@@ -1,10 +1,64 @@
-const sketchContainer = document.querySelector('.container')
+let color = 'black'
 
-for (let i = 1; i < 257; i++) {
-    const div = document.createElement('div')
-    div.setAttribute('id', 'theSquares')
-    sketchContainer.append(div)
+document.addEventListener('DOMContentLoaded', function () {
+    createBoard(16)
+    // getSize() called when selsct is clicked
+    let btn_popup = document.querySelector('#popup')
+    btn_popup.addEventListener('click', function () {
+        let size = getSize()
+        createBoard(size)
+    }
+
+    )
+    console.log('hi')
+})//waits for th css to load to prevent your website from crashing
+
+function createBoard(size) {
+    let board = document.querySelector('.board');
 
 
+    board.style.gridTemplateColumns = `repeat(${size},1fr)`;
+    board.style.gridTemplateRows = `repeat(${size},1fr)`;
 
+    let numDivs = size * size
+    for (let i = 0; i < numDivs; i++) {
+        let div = document.createElement('div');
+        div.addEventListener('mouseover', colorDiv)
+
+        board.insertAdjacentElement('beforeend', div)
+    }
+
+}
+
+function getSize() {
+    let input = prompt("what size of board do you want?")
+    let message = document.querySelector('#message')
+    if (input === "") {
+        message.textContent = "please provide number"
+    } else if (input < 0 || input > 100) {
+        message.textContent = "please provide a valid number (0-100)"
+    } else {
+        message.textContent = "now you can play!"
+        return input
+    }
+}
+
+function colorDiv() {
+    if (color == 'random') {
+        this.style.background = `hsl(${Math.random() * 360},100% ,50%)`
+    } else {
+        this.style.background = 'black'
+
+    }
+
+}
+
+function setColor(colorChoice) {
+    color = colorChoice
+
+}
+
+function resetBoard() {
+    let divs = document.querySelectorAll('div')
+    divs.forEach((div) => div.style.backgroundColor = 'white')
 }
