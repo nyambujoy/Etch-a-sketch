f140e9a9032fc218d17589dba7b2035c0881d4e9
nyambujoy <joyliznyambura@gmail.com>
2023-09-25 11:39:47 +0300

    styling the page (css,bootstrap,html,and js)

---
+++ app.js
@@ -0,0 +1,10 @@
+const sketchContainer = document.querySelector('.container')
+
+for (let i = 1; i < 257; i++) {
+    const div = document.createElement('div')
+    div.setAttribute('id', 'theSquares')
+    sketchContainer.append(div)
+
+
+
+}
